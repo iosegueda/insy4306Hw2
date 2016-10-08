03e06fcc7b2b5aa7ea416ade552f7ba34788ad95
Iris Osegueda <iriosegueda@gmail.com>
2016-10-08 02:18:24 -0500

    added open to menu and attached jfilechooser to it

--- Hw2.java
+++ Hw2.java
@@ -8,26 +8,58 @@ public class Hw2 extends JFrame //implements ActionListener
 {
 	private JDesktopPane desktop; 
 			Container container;
-			//JMenuBar menuBar;
-			/*JMenu file;
-			JMenu find;
-			JMenu about; 
-			JMenuItem exit; 
+			JMenuBar menuBar;
+			JMenu file;
 			JMenuItem open;
+			JMenuItem exit; 
+			/*JMenu find;
+			JMenu about; 
+			
+			
 			JMenuItem wordCount;
 			JMenuItem fileStats;*/
 		
 	public Hw2()
 	{
 		super( "GUI App" );
-		desktop = new JDesktopPane();
-		
-        try
+		try
         {
             UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
         }
 		catch ( Exception e ) {}
+		
+		container = getContentPane();
+		menuBar = new JMenuBar();
+		file = new JMenu( "File" );
+        file.setMnemonic( 'F' );
+        
+		JMenuItem open= new JMenuItem("Open");
+		open.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ae)
+			{
+				File fileName = readFile();
+				
+			}
+		});
+        file.add(open);
+		
+		exit=new JMenuItem("Exit");
+		exit.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent ae)
+			{
+				
+				System.exit(0);
+			}
+			
+		});
+		file.add(exit);
 
+		menuBar.add(file);
+		setJMenuBar( menuBar );
+		desktop = new JDesktopPane();
+		container.add( desktop );
 		
 		
 	}
@@ -37,8 +69,26 @@ public class Hw2 extends JFrame //implements ActionListener
 		Hw2 demo= new Hw2();
 		demo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		demo.setLocationRelativeTo(null);
-		demo.setSize(800,390);
+		demo.setSize(800,390); 
 		demo.setVisible(true);
 		
+	}
+	
+	public File readFile()
+	{
+		JFileChooser fileChooser = new JFileChooser();//create filechooser
+					
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		
+		int result= fileChooser.showOpenDialog(this);
+		//popup as a dialog on this JFRAme
+		
+		if(result == JFileChooser.CANCEL_OPTION)
+			System.exit(0);//if cancel exit
+		
+		File fileName = fileChooser.getSelectedFile();//else if select a file open that file
+		System.out.println(fileName);
+		return fileName;
+						
 	}
 }
